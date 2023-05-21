9a4696ca015265d6a6ef9d9c58022659d40d1cfa
Agnė Švambarytė <s037681@ad.viko.lt>
2023-05-21 12:31:46 +0300

    negative login test case

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -6,43 +6,69 @@ from pages.login_page import Login
 import time
 from utilities.read_properties import ReadConfig
 
+
 class Test_Auth:
 
     baseURL = ReadConfig.getWebUrl()
     email = ReadConfig.getEmail()
     password= ReadConfig.getPass()
+    bademail = ReadConfig.getBadEmail()
+    badpassword= ReadConfig.getBadPass()
 
-    def test_homepage(self,setup):
-        
-        self.driver = setup
-        self.driver.get(self.baseURL)
-        act_title = self.driver.title
+
+    # def test_homepage(self,setup):
+    
+    #     self.driver = setup
+    #     self.driver.get(self.baseURL)
+    #     act_title = self.driver.title
  
-        if act_title == "Prisijungimas - Knygos.lt":
-            assert True
-            self.driver.close()
-            self.driver.quit()
-        else:
-            self.driver.save_screenshot('.\\screenshots'+'test_homepage.png')
-            self.driver.close()
-            self.driver.quit()
-            assert False
+    #     if act_title == "Prisijungimas - Knygos.lt":
+    #         assert True
+    #         self.driver.close()
+    #         self.driver.quit()
+    #     else:
+    #         self.driver.save_screenshot('.\\screenshots'+'test_homepage.png')
+    #         self.driver.close()
+    #         self.driver.quit()
+    #         assert False
 
 
-    def test_login(self,setup):
+    # def test_login(self,setup):
+       
+    #     self.driver = setup
+    #     self.driver.get(self.baseURL)
+    #     self.login_page= Login(self.driver)
+
+    #     self.login_page.setEmail(self.email)
+    #     self.login_page.setPass(self.password)
+    #     self.login_page.clickLogin()
+      
+    #     self.login_page.clickProfile()
+    #     time.sleep(5)
+    
+    #     act_title= self.driver.find_element(By.XPATH ,"//input[@id='form_user_firstname']").get_attribute("value")
+    #     if act_title == self.password :
+    #         self.driver.quit()
+    #         assert True
+    #     else:
+    #         self.driver.quit()
+    #         assert False
+
+    #login negative case
+    def test_invalid_login(self,setup):
+           
         self.driver = setup
         self.driver.get(self.baseURL)
         self.login_page= Login(self.driver)
 
-        self.login_page.setEmail(self.email)
-        self.login_page.setPass(self.password)
+        self.login_page.setEmail(self.bademail)
+        self.login_page.setPass(self.badpassword)
         self.login_page.clickLogin()
-      
-        self.login_page.clickProfile()
+
         time.sleep(5)
     
-        act_title= self.driver.find_element(By.XPATH ,"//input[@id='form_user_firstname']").get_attribute("value")
-        if act_title == self.password :
+        invalid_error= self.driver.find_element(By.XPATH,'/html[1]/body[1]/main[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]').text
+        if invalid_error == "Klaidingi duomenys." :
             self.driver.quit()
             assert True
         else:
--- utilities/read_properties.py
+++ utilities/read_properties.py
@@ -13,8 +13,19 @@ class ReadConfig():
     def getEmail():
         email= config.get('common information', 'email')
         return email
+    
+    @staticmethod
+    def getBadEmail():
+        email= config.get('common information', 'bademail')
+        return email
 
     @staticmethod
     def getPass():
         password= config.get('common information', 'password')
+        return password
+    
+
+    @staticmethod
+    def getBadPass():
+        password= config.get('common information', 'badpassword')
         return password
